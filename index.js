ffd970b94e63c0bad69edc87c596855830c9a9cc
Meghana1603 <meghanamannem163@gmail.com>
2021-08-25 23:17:58 +0530

    book get api

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
+//database
+const Database = require("./database");
+
 const OurApp = express();
 
 OurApp.get("/", (request, response)=> {
-    response.json({message: "Request Served"});
+    response.json({message: "Server is working"});
+});
+
+OurApp.get("/book",(req,res) => {
+    return res.json({books: Database.Book});
 });
 
 OurApp.listen(4000, ()=> console.log("server running"));
