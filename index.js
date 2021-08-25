e03365c8b3754811eff217645e651c152666c78e
Meghana1603 <meghanamannem163@gmail.com>
2021-08-25 22:58:51 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const OurApp = express();
+
+OurApp.get("/", (request, response)=> {
+    response.json({message: "Request Served"});
+});
+
+OurApp.listen(4000, ()=> console.log("server running"));
